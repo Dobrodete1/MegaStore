8a038e25c7fdccf7e5fda253d2697ae59e08135e
Dobrodete1 <kozho31203@gmail.com>
2022-07-08 11:41:25 +0600

    fix: function

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-const surnameData = document.querySelector('#surnameData');
-const nameData = document.querySelector('#nameData');
-const patronymicData = document.querySelector('#patronymicData');
-const dateData = document.querySelector('#dateData');
-const adressData = document.querySelector('#adressData');
-const pasportNumberData = document.querySelector('#pasportNumberData');
-const phoneNumber = document.querySelector('#phoneNumber');
-const mailData = document.querySelector('#mailData');
-const bankNameData = document.querySelector('#bankNameData');
-const placeOfLivingData = document.querySelector('#placeOfLivingData');
+const profile = document.querySelector('#profile');
 
+const getInfoProfile = () => {
+    const userSurnameData = document.querySelector('#surnameData');
+    const userNameData = document.querySelector('#nameData');
+    const userPatronymicData = document.querySelector('#patronymicData');
+    const userBithdayData = document.querySelector('#dateData');
+    const userAdressData = document.querySelector('#adressData');
+    const userPasportNumberData = document.querySelector('#pasportNumberData');
+    const userPhoneNumber = document.querySelector('#phoneNumber');
+    const userMailData = document.querySelector('#mailData');
+    const userBankNameData = document.querySelector('#bankNameData');
+    const userPlaceOfLivingData = document.querySelector('#placeOfLivingData');
 
+    console.log(`Фамилия:${userSurnameData.value}`);
+    console.log(`Имя:${userNameData.value}`);
+    console.log(`ОТчество: ${userPatronymicData.value}`);
+    console.log(`дата рождения:${userBithdayData.value}`);
+    console.log(`Место проживание:${userAdressData.value}`);
+    console.log(`Номер паспорта:${userPasportNumberData}`);
+    console.log(`Номер телефона : ${userPhoneNumber}`);
+    console.log(`Почта:${userMailData}`);
+    console.log(`Наименование банка :${userBankNameData}`);
+    console.log(`Фактическое место проживания:${userPlaceOfLivingData}`);
+} 
 
-
+profile.addEventListener( 'submit', getInfoProfile);
 
 
 
