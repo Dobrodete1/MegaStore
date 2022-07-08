8fe8d268eaa4c69fc77da50c10148ad1c3d4f12e
Dobrodete1 <kozho31203@gmail.com>
2022-07-08 10:17:00 +0600

    fiet: const for Id

---
+++ script.js
@@ -0,0 +1,51 @@
+const surnameData = document.querySelector('#surnameData');
+const nameData = document.querySelector('#nameData');
+const patronymicData = document.querySelector('#patronymicData');
+const dateData = document.querySelector('#dateData');
+const adressData = document.querySelector('#adressData');
+const pasportNumberData = document.querySelector('#pasportNumberData');
+const phoneNumber = document.querySelector('#phoneNumber');
+const mailData = document.querySelector('#mailData');
+const bankNameData = document.querySelector('#bankNameData');
+const placeOfLivingData = document.querySelector('#placeOfLivingData');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const nameCompany = document.querySelector('#nameCompany');
+const positionData = document.querySelector('#positionData');
+const wageMounthData = document.querySelector('#wageMounthData');
+const wageTreeMounthData = document.querySelector('#wageTreeMounthData');
+const productNameData = document.querySelector('#productNameData');
+const productPriceData = document.querySelector('#productPriceData');
+const productAmountData = document.querySelector('#productAmountData');
+const creditAmountData = document.querySelector('#creditAmountData');
+const ternCreditData = document.querySelector('#ternCreditData');
+const receivingPointData = document.querySelector('#receivingPointData');
