4f796e2d51ea28016138f163fc113e6c1923dd85
Dobrodete1 <kozho31203@gmail.com>
2022-07-08 12:07:21 +0600

    feat: second function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const profile = document.querySelector('#profile');
+const profile = document.querySelector('#getInfoProfile');
 
 const getInfoProfile = () => {
     const userSurnameData = document.querySelector('#surnameData');
@@ -10,6 +10,7 @@ const getInfoProfile = () => {
     const userPhoneNumber = document.querySelector('#phoneNumber');
     const userMailData = document.querySelector('#mailData');
     const userBankNameData = document.querySelector('#bankNameData');
+    const passportTakeDateData = document.querySelector('#takeDateData');
     const userPlaceOfLivingData = document.querySelector('#placeOfLivingData');
 
     console.log(`Фамилия:${userSurnameData.value}`);
@@ -17,14 +18,44 @@ const getInfoProfile = () => {
     console.log(`ОТчество: ${userPatronymicData.value}`);
     console.log(`дата рождения:${userBithdayData.value}`);
     console.log(`Место проживание:${userAdressData.value}`);
-    console.log(`Номер паспорта:${userPasportNumberData}`);
-    console.log(`Номер телефона : ${userPhoneNumber}`);
-    console.log(`Почта:${userMailData}`);
-    console.log(`Наименование банка :${userBankNameData}`);
-    console.log(`Фактическое место проживания:${userPlaceOfLivingData}`);
+    console.log(`Номер паспорта:${userPasportNumberData.value}`);
+    console.log(`Номер телефона : ${userPhoneNumber.value}`);
+    console.log(`Почта:${userMailData.value}`);
+    console.log(`Наименование банка :${userBankNameData.value}`);
+    console.log(`Фактическое место проживания:${userPlaceOfLivingData.value}`);
+    console.log(`Дата получение паспорта:${passportTakeDateData.value}`);
 } 
 
-profile.addEventListener( 'submit', getInfoProfile);
+profile.addEventListener( 'click', getInfoProfile);
+
+
+const registerCredit = document.querySelector('#getInfocreditsButton');
+
+const getInfoCredit = () =>{
+    const nameCompany = document.querySelector('#nameCompany');
+    const positionData = document.querySelector('#positionData');
+    const wageMounthData = document.querySelector('#wageMounthData');
+    const wageTreeMounthData = document.querySelector('#wageTreeMounthData');
+    const productNameData = document.querySelector('#productNameData');
+    const productPriceData = document.querySelector('#productPriceData');
+    const productAmountData = document.querySelector('#productAmountData');
+    const creditAmountData = document.querySelector('#creditAmountData');
+    const ternCreditData = document.querySelector('#ternCreditData');
+    const receivingPointData = document.querySelector('#receivingPointData');
+
+    console.log(`Наименование организации:${nameCompany.value}`);
+    console.log(`Должность:${positionData.value}`);
+    console.log(`Заработная плата в месяц:${wageMounthData.value}`);
+    console.log(`Заработная плата за посл. 3 месяца:${wageTreeMounthData}`);
+    console.log(`Покупка телефона (наименования товара):${productNameData.value}`);
+    console.log(`Стоимость товара:${productPriceData.value}`);
+    console.log(`количество товара:${productAmountData.value}`);
+    console.log(`Итог сумма кредита: ${creditAmountData.value}`);
+    console.log(`Срок кредита (мес.):${ternCreditData.value}`);
+    console.log(`Точка получения товара: ${receivingPointData.value}`);
+};
+
+registerCredit.addEventListener('click' , getInfoCredit)
 
 
 
@@ -49,16 +80,3 @@ profile.addEventListener( 'submit', getInfoProfile);
 
 
 
-
-
-
-const nameCompany = document.querySelector('#nameCompany');
-const positionData = document.querySelector('#positionData');
-const wageMounthData = document.querySelector('#wageMounthData');
-const wageTreeMounthData = document.querySelector('#wageTreeMounthData');
-const productNameData = document.querySelector('#productNameData');
-const productPriceData = document.querySelector('#productPriceData');
-const productAmountData = document.querySelector('#productAmountData');
-const creditAmountData = document.querySelector('#creditAmountData');
-const ternCreditData = document.querySelector('#ternCreditData');
-const receivingPointData = document.querySelector('#receivingPointData');
